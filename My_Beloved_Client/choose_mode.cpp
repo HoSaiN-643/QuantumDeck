36f492ccfc3d91231f6a9b39ef27f59e94f39bfd
Mahdi Farahrouz <mahdyfarahrooz1993@gmail.com>
2025-06-29 09:43:53 +0330

    second choose_mode

--- My_Beloved_Client/choose_mode.cpp
+++ My_Beloved_Client/choose_mode.cpp
@@ -1,14 +1,39 @@
 #include "choose_mode.h"
 #include "ui_choose_mode.h"
 
+// Constructor: Initializes the main window and sets up the UI
 Choose_mode::Choose_mode(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::Choose_mode)
 {
     ui->setupUi(this);
+    
+    // Connect buttons to slots
+    connect(ui->fourPlayerButton, &QPushButton::clicked, this, &Choose_mode::onFourPlayerClicked);
+    connect(ui->twoPlayerButton, &QPushButton::clicked, this, &Choose_mode::onTwoPlayerClicked);
+    connect(ui->exitButton, &QPushButton::clicked, this, &Choose_mode::onExitClicked);
 }
 
+// Destructor: Cleans up the UI
 Choose_mode::~Choose_mode()
 {
     delete ui;
+}
+
+// Slot for 4 Player button click
+void Choose_mode::onFourPlayerClicked()
+{
+    // Implement 4 player online mode logic
+}
+
+// Slot for 2 Player button click
+void Choose_mode::onTwoPlayerClicked()
+{
+    // Implement 2 player online mode logic
+}
+
+// Slot for Exit button click
+void Choose_mode::onExitClicked()
+{
+    close(); // Close the application
 }
--- My_Beloved_Client/choose_mode.h
+++ My_Beloved_Client/choose_mode.h
@@ -3,20 +3,31 @@
 
 #include <QMainWindow>
 
-namespace Ui {
-class Choose_mode;
-}
+QT_BEGIN_NAMESPACE
+namespace Ui { class Choose_mode; }
+QT_END_NAMESPACE
 
+// Main window class for online mode selection
 class Choose_mode : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit Choose_mode(QWidget *parent = nullptr);
+    // Constructor
+    Choose_mode(QWidget *parent = nullptr);
+    // Destructor
     ~Choose_mode();
 
+private slots:
+    // Slot for 4 player mode button
+    void onFourPlayerClicked();
+    // Slot for 2 player mode button
+    void onTwoPlayerClicked();
+    // Slot for exit button
+    void onExitClicked();
+
 private:
-    Ui::Choose_mode *ui;
+    Ui::Choose_mode *ui; // UI pointer
 };
 
 #endif // CHOOSE_MODE_H
