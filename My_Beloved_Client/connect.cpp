3b8b4ece2aff5991b35a4d9cfa8a1932638de00f
Mahdi Farahrouz <mahdyfarahrooz1993@gmail.com>
2025-06-30 12:18:11 +0330

    third connect

--- My_Beloved_Client/connect.cpp
+++ My_Beloved_Client/connect.cpp
@@ -2,49 +2,53 @@
 #include "ui_connect.h"
 #include "client.h"
 #include "log.h"
+#include "player.h"
 #include <QHostAddress>
 #include <QMessageBox>
 #include <QDebug>
 
+// Constructor: Initializes the main window and sets up the UI
 Connect::Connect(Client *client, QWidget *parent)
     : QMainWindow(parent),
-    ui(new Ui::connect),
-    client(client),
-    logWindow(new Log(client, this))
+      ui(new Ui::connect),
+      client(client),
+      player(),  // Default-constructed Player
+      logWindow(new Log(player, client, this))
 {
     ui->setupUi(this);
     connect(ui->Connect_Btn, &QPushButton::clicked, this, &Connect::on_Connect_Btn_clicked);
 
     connect(client, &Client::ConnectedToServer,
-            this,   &Connect::OnConnected);
+            this, &Connect::OnConnected);
     connect(client, &Client::ErrorOccurred,
-            this,   &Connect::OnErrorOccurred);
+            this, &Connect::OnErrorOccurred);
 }
 
+// Destructor: Cleans up the UI
 Connect::~Connect()
 {
     delete ui;
-    // logWindow به عنوان child والد (this) پاک می‌شود
+    // logWindow is automatically deleted as a child
 }
 
+// Slot for connect button click
 void Connect::on_Connect_Btn_clicked()
 {
-
-    int  port   = ui->Port_line->text().toInt();
-
-    QHostAddress host(QString(ui->Address_line->text().trimmed()));
-
+    int port = ui->Port_line->text().toInt();
+    QHostAddress host(ui->Address_line->text().trimmed());
 
     qDebug() << "Connecting to" << host.toString() << ":" << port;
     client->ConnectToServer(host, quint16(port));
 }
 
+// Slot for successful server connection
 void Connect::OnConnected()
 {
     this->hide();
     logWindow->show();
 }
 
+// Slot for handling connection errors
 void Connect::OnErrorOccurred(const QString &errorString)
 {
     QMessageBox::critical(this, "Connection Error", errorString);
--- My_Beloved_Client/connect.h
+++ My_Beloved_Client/connect.h
@@ -2,9 +2,11 @@
 #define CONNECT_H
 
 #include <QMainWindow>
+#include "player.h"  // Include player.h for Player class
+
 namespace Ui { class connect; }
 
-// فقط اعلام
+// Forward declarations
 class Client;
 class Log;
 
@@ -13,17 +15,23 @@ class Connect : public QMainWindow
     Q_OBJECT
 
 public:
+    // Constructor
     explicit Connect(Client *client, QWidget *parent = nullptr);
+    // Destructor
     ~Connect();
 
 private slots:
+    // Slot for successful server connection
     void OnConnected();
+    // Slot for connect button click
     void on_Connect_Btn_clicked();
+    // Slot for handling connection errors
     void OnErrorOccurred(const QString &errorString);
 
 private:
     Ui::connect *ui;
     Client      *client;
+    Player      player;  // Added Player member
     Log         *logWindow;
 };
 
